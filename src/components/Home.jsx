7e0a478f23e93f305e3b7873c1abbddde74f84f2
ppasszzw311 <ppasszzw311@gmail.com>
2025-04-13 10:34:51 +0800

    add log

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -9,6 +9,7 @@ const Home = () => {
         axios.get("/api/weatherforecast")
         .then((res) => {
             setData(res.data);
+            console.log(res);
         })
         .catch((err) => {
             console.log(err);
