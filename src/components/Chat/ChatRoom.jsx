a37b156295792222366d3fba99770cdcb83f70a5
ppasszzw311 <ppasszzw311@gmail.com>
2025-05-13 17:49:23 +0800

    更新測是

---
+++ src/components/Chat/ChatRoom.jsx
@@ -0,0 +1,72 @@
+import React, { useState, useEffect } from "react";
+import * as signalR from "@microsoft/signalr";
+
+// 建立聊天室
+const ChatRoom = () => {
+    const [connection, setConnection] = useState(null);
+    const [messages, setMessages] = useState([]);
+    const [user, setUser] = useState("");
+    const [message, setMessage] = useState("");
+
+    useEffect(() => {
+        const API_URL = import.meta.env.VITE_API_URL;
+        const newConnection = new signalR.HubConnectionBuilder()
+            .withUrl(`${API_URL}/charhub`)
+            .withAutomaticReconnect()
+            .build();
+
+        setConnection(newConnection);
+    }, []);
+
+    useEffect(() => {
+        if (connection) {
+            connection.start()
+                .then(() => {
+                    console.log("Connected to chat hub");
+
+                    connection.on("ReceiveMessage", (user, message) => {
+                        setMessages((prevMessages) => [...prevMessages, { user, message }]);
+                    });
+                })
+                .catch((err) => {
+                    console.error("Error connecting to chat hub:", err);
+                });
+            
+        }
+    }, [connection]);
+
+    const sendMessage = () => {
+        if (connection && connection.state === signalR.HubConnectionState.Connected) {
+            try {
+                connection.invoke("SendMessage", user, message);
+                setMessage("");
+            } catch (err) {
+                console.error("Error sending message:", err);
+            }
+        }
+    };
+
+    return (
+        <div>
+            <h2>聊天室</h2>
+            <input placeholder="你的姓名" value={user} onChange={(e) = setUser(e.target.value)} />
+
+            <input placeholder="說說話" value={message} onChange={(e) => setMessage(e.target.value)} />
+
+            <button onClick={sendMessage}>送出</button>
+
+            <div>
+                <ul>
+                    {
+                        messages.map((msg, idx) => (
+                            <li key={idx}>{msg.user}: {msg.message}</li>
+                        ))
+                    }
+                </ul>
+            </div>
+        </div>
+    )
+}
+
+export default ChatRoom;
+    
